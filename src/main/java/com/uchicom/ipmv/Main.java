13f269c2143db3beb04dc688709405f984a17455
Shigeki Uchiyama <shigeki.uchiyama@uchicom.com>
2018-05-24 10:49:40 +0900

    #1 IpmAdapterを利用する

--- src/main/java/com/uchicom/ipmv/Main.java
+++ src/main/java/com/uchicom/ipmv/Main.java
@@ -43,37 +43,52 @@ public class Main extends Application {
 			launch();
 		}
 	}
-	private static final int WIDTH = 100;
-	private static final int HEIGHT = 100;
+
+	private static final int WIDTH = 500;
+	private static final int HEIGHT = 500;
 
 	WritableImage image = new WritableImage(WIDTH, HEIGHT);
 
 	Canvas canvas = new Canvas(WIDTH, HEIGHT);
 	GraphicsContext gc = canvas.getGraphicsContext2D();
 
+	private boolean dragg;
+	private boolean angle;
+	private double tx;
+	private double ty;
+	private double tz;
+	private double stageX;
+	private double stageY;
+	private double sx;
+	private double sy;
+	private double x;
+	private double y;
+	private double z;
+
 	@Override
 	public void start(Stage stage) {
 		Group root = new Group();
 
-		//オブジェクト設定
+		// オブジェクト設定
 		Sphere sphere = new Sphere(20);
 		Box myBox = new Box(WIDTH, HEIGHT, 1);
-		
-		//テクスチャ設定
+
+		// テクスチャ設定
 		final PhongMaterial material = new PhongMaterial();
 		material.setDiffuseMap(image);
-		
+
 		sphere.setMaterial(material);
 		myBox.setMaterial(material);
 		myBox.setScaleZ(0.5);
 		myBox.setScaleY(0.5);
 		myBox.setScaleZ(0.5);
 
-//		root.getChildren().add(sphere);
+		// root.getChildren().add(sphere);
 		root.getChildren().add(myBox);
-		
+
 		Scene scene = new Scene(root, 320, 320, true, SceneAntialiasing.BALANCED);
-		//カメラ3D
+
+		// カメラ3D
 		PerspectiveCamera camera = new PerspectiveCamera(true);
 		camera.setFieldOfView(80);
 		camera.setTranslateZ(-100.001);
@@ -84,21 +99,63 @@ public class Main extends Application {
 		stage.setTitle("Hello, JavaFX 3D World!");
 		stage.setOpacity(0.5);
 		stage.setAlwaysOnTop(true);
-		
-		//枠削除
+
+		// 枠削除
 		stage.initStyle(StageStyle.TRANSPARENT);
 		stage.getScene().setFill(null);
 		stage.getScene().getRoot().setStyle("-fx-background-color: transparent");
-		
+
+		scene.setOnMousePressed(value -> {
+			if (value.isControlDown()) {
+				angle = true;
+				x = value.getX();
+				y = value.getY();
+				z = value.getZ();
+				tx = camera.getTranslateX();
+				ty = camera.getTranslateY();
+				tz = camera.getTranslateZ();
+			} else {
+				dragg = true;
+				stageX = stage.getX();
+				stageY = stage.getY();
+				sx = value.getScreenX();
+				sy = value.getScreenY();
+				System.out.println("1x,y,z=" + x + "," + y + "," + z);
+			}
+		});
+		scene.setOnMouseDragged(value -> {
+			if (dragg) {
+
+				double dx = value.getScreenX() - sx;
+				double dy = value.getScreenY() - sy;
+				stage.setX(stageX + dx);
+				stage.setY(stageY + dy);
+				System.out.println("2x,y,z=" + x + "," + y + "," + z);
+			} else if (angle) {
+				double dx = value.getScreenX() - sx;
+				double dy = value.getScreenY() - sy;
+				camera.setTranslateX(tx + dx / 10);
+				camera.setTranslateY(ty + dy / 10);
+//				camera.setTranslateZ(camera.getTranslateZ() + dz / 10);
+			}
+		});
+		scene.setOnMouseReleased(value -> {
+			dragg = false;
+			System.out.println("setOnMouseReleased");
+		});
+		scene.setOnMouseDragExited(value -> {
+			dragg = false;
+			System.out.println("setOnMouseDragExited");
+		});
 		stage.show();
-		//テクスチャが同的に変えられるかの確認スレッド
+		// テクスチャが同的に変えられるかの確認スレッド
 		Thread thread = new Thread(() -> {
 			while (true) {
 				PixelWriter writer = image.getPixelWriter();
 				try {
 					Thread.sleep(1000);
-					//描画スレッドはこれを使う。
-					Platform.runLater(() ->{
+					// 描画スレッドはこれを使う。
+					Platform.runLater(() -> {
 						drawShapes(gc);
 						WritableImage temp = canvas.snapshot(null, null);
 						WritablePixelFormat<IntBuffer> pf = WritablePixelFormat.getIntArgbInstance();
@@ -118,18 +175,22 @@ public class Main extends Application {
 	}
 
 	int i = 0;
+
 	/**
 	 * テクスチャ描画確認
+	 * 
 	 * @param gc
 	 */
 	private void drawShapes(GraphicsContext gc) {
-//		gc.setFontSmoothingType(FontSmoothingType.LCD);
+		// gc.setFontSmoothingType(FontSmoothingType.LCD);
 		gc.setFill(Paint.valueOf("#FF0000"));
 		gc.fillRect(0, 0, WIDTH, HEIGHT);
 		gc.setFill(Paint.valueOf("#FFFFFF"));
-//		gc.clearRect(0, 0, 100, 100);
-		gc.fillText(String.valueOf(i++), WIDTH / 2, HEIGHT / 2);
-
+		// gc.clearRect(0, 0, 100, 100);
+		for (int j = 0; j < 10; j++) {
+			//受信したメッセージを縦に流れるようにする。
+		gc.fillText(String.valueOf(i++) + ":あいうえお", WIDTH / 2, HEIGHT / 2+j * 15);
+		}
 	}
 
 }
--- src/main/java/com/uchicom/ipmv/window/IpmFrame.java
+++ src/main/java/com/uchicom/ipmv/window/IpmFrame.java
@@ -17,7 +17,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 
-import com.uchicom.ipm.Ipm;
+import com.uchicom.ipm.IpmAdapter;
 import com.uchicom.ipm.Message;
 import com.uchicom.ipm.dto.User;
 import com.uchicom.ipm.type.Mode;
@@ -33,10 +33,6 @@ import com.uchicom.ipmv.util.IpmUserListModel;
  */
 public class IpmFrame extends JFrame {
 
-	private static String USER = "user1";
-	private static String HOST = "host1";
-	
-	private static String NICKNAME = "hoge";
 
 	// Ver(1) : Packet番号 : 自User名 : 自Host名 : Command番号 : 追加部
 	/**
@@ -48,7 +44,7 @@ public class IpmFrame extends JFrame {
 	private JList<User> userList = new JList<>();
 	private JCheckBox checkbox = new JCheckBox();
 
-	private Ipm messanger = new Ipm(10000+(int)System.currentTimeMillis() % 10000);
+	private IpmAdapter ipmAdapter = new IpmAdapter("user1", "host1", "hoge", "groupName", false);
 
 	public IpmFrame() {
 		super("ipmv");
@@ -57,201 +53,38 @@ public class IpmFrame extends JFrame {
 
 	/**
 	 * ipmの初期化を実施.
+	 * クライアントアプリによって作り方が変わる.
 	 */
 	private void initIpm() {
-
 		// ここではリスナーの登録を実施。
 		try {
-			// メッセージ送信、受信以外をあらかじめ自動でやってくれるAdapterかDefaultも用意する。
-			//サービスにエントリ（起動時にBroadcast）
-			messanger.addListener(Mode.IPMSG_BR_ENTRY, message -> {
-				System.out.println(message);
-				User user = messanger.addUser(new User.Builder(message).nickName(message.getBody()).build());
-				Message.Builder builder = new Message.Builder(user)
-						.packetNo(messanger.issuePacketNo())
-						.mode(Mode.IPMSG_ANSENTRY)
-						.hostName(HOST)
-						.userName(USER)
-						.extra(NICKNAME);
-				if (checkbox.isSelected()) {
-					builder.option(Option.IPMSG_ABSENCEOPT);
-				}
-				//.extra() // ニックネームとか
-				messanger.sendDelay(builder
-				.build());
+			//メッセージ受信
+			ipmAdapter.addListener(Mode.IPMSG_SENDMSG, message -> {
+				MessageDialog dialog = new MessageDialog(this, false, message.is(Option.IPMSG_SECRETOPT), Arrays.asList(ipmAdapter.getUser(new User.Builder(message).build())), message.getPacketNo(), message.getBody());
+				dialog.setVisible(true);
 			});
-			//エントリを認識したことを通知
-			messanger.addListener(Mode.IPMSG_ANSENTRY, message -> {
-				System.out.println(message);
-				messanger.addUser(new User.Builder(message).nickName(message.getBody()).build());
-			});
-			//メッセージ返却
-			messanger.addListener(Mode.IPMSG_RECVMSG, message -> {
-				System.out.println("送信確認->" + message);
+			//メッセージ受信通知
+			ipmAdapter.addListener(Mode.IPMSG_RECVMSG, message -> {
 				long packetNo = Long.parseLong(message.getBody().trim());
 				MessageDialog dialog = dialogMap.get(packetNo);
 				dialogMap.remove(packetNo);
 				dialog.dispose();
 				System.out.println("dispose!");
 			});
-			//メッセージ受信
-			messanger.addListener(Mode.IPMSG_SENDMSG, message -> {
-				System.out.println("メッセージ->" + message.getBody());
-				if (message.is(Mode.IPMSG_SENDMSG, Option.IPMSG_SENDCHECKOPT)) {
-					messanger.send(new Message.Builder(new User.Builder(message).build())
-							.packetNo(messanger.issuePacketNo())
-							.mode(Mode.IPMSG_RECVMSG)
-							.option(Option.IPMSG_AUTORETOPT)
-							.hostName(HOST)
-							.userName(USER)
-							.extra(String.valueOf(message.getPacketNo()))
-							.build());
-				}
-				MessageDialog dialog = new MessageDialog(this, false, message.is(Option.IPMSG_SECRETOPT), Arrays.asList(messanger.getUser(new User.Builder(message).build())), message.getPacketNo(), message.getBody());
-				dialog.setVisible(true);
-			});
-			//無操作
-			messanger.addListener(Mode.IPMSG_NOOPERATION, message -> {
-				System.out.println(message);
-			});
-			//不在モード変更
-			messanger.addListener(Mode.IPMSG_BR_ABSENCE, message -> {
-				System.out.println(message);
-				messanger.changeUser(new User.Builder(message).build());
-			});
-
-			//ホストリスト送出可能メンバの探索
-			messanger.addListener(Mode.IPMSG_BR_ISGETLIST, message -> {
-				System.out.println(message);
-			});
-			//ホストリスト送出可能通知
-			messanger.addListener(Mode.IPMSG_OKGETLIST , message -> {
-				System.out.println(message);
-				messanger.send(new Message.Builder(new User.Builder(message).build())
-						.packetNo(messanger.issuePacketNo())
-						.mode(Mode.IPMSG_GETLIST)
-						.hostName(HOST)
-						.userName(USER)
-						.build());
-			});
-			//ホストリスト送出要求
-			messanger.addListener(Mode.IPMSG_GETLIST, message -> {
-				System.out.println(message);
-			});
-			//ホストリスト送出
-			messanger.addListener(Mode.IPMSG_ANSLIST, message -> {
-				System.out.println(message);
-			});
-			
 			//封書の開封通知
-			messanger.addListener(Mode.IPMSG_READMSG, message -> {
-				System.out.println(message);
+			ipmAdapter.addListener(Mode.IPMSG_READMSG, message -> {
 				JOptionPane.showMessageDialog(this, "開封されました");
 			});
-			//封書破棄通知
-			messanger.addListener(Mode.IPMSG_DELMSG, message -> {
-				System.out.println(message);
+			//封書の破棄通知
+			ipmAdapter.addListener(Mode.IPMSG_DELMSG, message -> {
 				JOptionPane.showMessageDialog(this, "破棄されました");
 			});
-			//封書の開封確認（8 版から追加）
-			messanger.addListener(Mode.IPMSG_ANSREADMSG, message -> {
-				System.out.println(message);
-			});
-			//IPMSGバージョン情報取得
-			messanger.addListener(Mode.IPMSG_GETINFO, message -> {
-				System.out.println(message);
-			});
-			//IPMSGバージョン情報応答
-			messanger.addListener(Mode.IPMSG_SENDINFO, message -> {
-				System.out.println(message);
-			});
-			//不在通知文取得
-			messanger.addListener(Mode.IPMSG_GETABSENCEINFO, message -> {
-				System.out.println(message);
-			});
-			//不在通知文応答
-			messanger.addListener(Mode.IPMSG_SENDABSENCEINFO, message -> {
-				System.out.println(message);
-			});
-			//サービスから抜ける（終了時にBroadcast）
-			messanger.addListener(Mode.IPMSG_BR_EXIT, message -> {
-				// ブロードキャストで受信する。
-				System.out.println("削除");
-				System.out.println(message);
-				messanger.removeUser(new User.Builder(message).build());
-			});
-			
-			messanger.addNetworkListener(broadcast->{
-				messanger.send(new Message.Builder(new User.BroadcastBuilder().build())
-						.packetNo(messanger.issuePacketNo())
-						.mode(Mode.IPMSG_BR_ISGETLIST)
-						.hostName(HOST)
-						.userName(USER)
-						.build());
-			});
-			messanger.start();
-
-			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-				try {
-					messanger.close();
-					System.out.println("shutdown hook close!");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}));
-			entryIpm();
+			ipmAdapter.entry();//リスナー登録後にentryを実施
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * エントリー
-	 */
-	private void entryIpm() {
-		Message.Builder builder = new Message.Builder(new User.BroadcastBuilder().build())
-				.packetNo(messanger.issuePacketNo())
-				.mode(Mode.IPMSG_BR_ENTRY)
-				.hostName(HOST)
-				.userName(USER)
-				.extra(NICKNAME);
-		if (checkbox.isSelected()) {
-			builder.option(Option.IPMSG_ABSENCEOPT);
-		}
-		messanger.send(builder
-				.build());
-	}
-
-	/**
-	 * 不在.
-	 */
-	private void absenceIpm() {
-
-		Message.Builder builder = new Message.Builder(new User.BroadcastBuilder().build())
-				.packetNo(messanger.issuePacketNo())
-				.mode(Mode.IPMSG_BR_ABSENCE)
-				.hostName(HOST)
-				.userName(USER)
-				.extra(NICKNAME);
-		if (checkbox.isSelected()) {
-			builder.option(Option.IPMSG_ABSENCEOPT);
-		}
-		messanger.send(builder
-				.build());
-	}
-	/**
-	 * 退出
-	 */
-	private void exitIpm() {
-		messanger.send(new Message.Builder(new User.BroadcastBuilder().build())
-				.packetNo(messanger.issuePacketNo())
-				.mode(Mode.IPMSG_BR_EXIT)
-				.hostName(HOST)
-				.userName(USER)
-				.extra(NICKNAME)
-				.build());
-	}
 
 	/**
 	 * コンポーネント初期化.
@@ -260,13 +93,13 @@ public class IpmFrame extends JFrame {
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
-				exitIpm();
+				ipmAdapter.exit();
 				dispose();
 				System.exit(0);
 			}
 		});
 		userList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-		userList.setModel(new IpmUserListModel(messanger));
+		userList.setModel(new IpmUserListModel(ipmAdapter.getIpm()));
 		userList.addListSelectionListener((event) -> {
 			System.out.println(event);
 			if (event.getValueIsAdjusting()) {
@@ -275,7 +108,7 @@ public class IpmFrame extends JFrame {
 		});
 		checkbox.setText("不在");
 		checkbox.addActionListener((e)->{
-			absenceIpm();
+			ipmAdapter.absence(checkbox.isSelected());
 		});
 		BorderLayout layout = new BorderLayout();
 		getContentPane().setLayout(layout);
@@ -297,29 +130,22 @@ public class IpmFrame extends JFrame {
 	}
 
 	/**
-	 * メッセージ送信.
+	 * メッセージ送信初回処理
 	 * @param user
 	 * @param text
 	 */
 	public void send(MessageDialog dialog, User user, String text, boolean secret, boolean b) {
-		long packetNo = messanger.issuePacketNo();
-		dialogMap.put(packetNo, dialog);
-		Message.Builder builder = new Message.Builder(user)
-				.packetNo(packetNo)
-				.mode(Mode.IPMSG_SENDMSG)
-				.hostName(HOST)
-				.userName(USER)
-				.option(Option.IPMSG_SENDCHECKOPT)
-				.extra(text);
-		if (secret) {
-			builder.option(Option.IPMSG_SECRETOPT);
-		}
-		send(dialog, builder.build());
+		Message message = ipmAdapter.createMessage(user, text, secret, b);
+		dialogMap.put(message.getPacketNo(), dialog);
+		send(dialog, message);
 	}
+	/**
+	 * メッセージ送信
+	 * @param dialog
+	 * @param message
+	 */
 	public void send(MessageDialog dialog, Message message) {
-		messanger.send(message);
-		//アラートスレッド
-		Thread alertThread = new Thread(()->{
+		ipmAdapter.send(message, ()->{
 			try {
 				Thread.sleep(3_000); // 3秒後
 				if (dialogMap.containsKey(message.getPacketNo())) {
@@ -329,22 +155,22 @@ public class IpmFrame extends JFrame {
 				e.printStackTrace();
 			}
 		});
-		alertThread.start();
 	}
 	
+	/**
+	 * リスト更新.
+	 */
 	public void update() {
-		messanger.clearUser();
-		
-		entryIpm();
+		ipmAdapter.update();
 	}
 	
+	/**
+	 * 通知.
+	 * @param mode
+	 * @param user
+	 * @param packetNo
+	 */
 	public void notify(Mode mode, User user, long packetNo) {
-		messanger.send(new Message.Builder(user)
-				.packetNo(messanger.issuePacketNo())
-				.mode(mode)
-				.hostName(HOST)
-				.userName(USER)
-				.extra(String.valueOf(packetNo))
-				.build());
+		ipmAdapter.notify(mode, user, packetNo);
 	}
 }
